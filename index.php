7052aef2e2ebd0043f02b465bec0dc8dfe36881f
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-06-07 09:36:46 +0200

    rename fork of "ungdomsforskning" theme to "tfu2"

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 
 /**
- * @defgroup plugins_themes_ungdomsforskning
+ * @defgroup plugins_themes_tfu2
  */
 
 /**
- * @file plugins/themes/ungdomsforskning/index.php
+ * @file plugins/themes/tfu2/index.php
  *
  * Copyright (c) 2013-2014 Simon Fraser University Library
  * Copyright (c) 2003-2014 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_themes_ungdomsforskning
+ * @ingroup plugins_themes_tfu2
  *
- * @brief Wrapper for "ungdomsforskning" theme plugin.
+ * @brief Wrapper for "tfu2" theme plugin.
  */
 
-require_once('ungdomsforskningThemePlugin.inc.php');
+require_once('tfu2ThemePlugin.inc.php');
 
-return new ungdomsforskningThemePlugin();
+return new tfu2ThemePlugin();
 
 ?>
--- tfu2ThemePlugin.inc.php
+++ tfu2ThemePlugin.inc.php
@@ -1,40 +1,40 @@
 <?php
 
 /**
- * @file plugins/themes/ungdomsforskning/ungdomsforskningThemePlugin.inc.php
+ * @file plugins/themes/tfu2/tfu2ThemePlugin.inc.php
  *
  * Copyright (c) 2013-2014 Simon Fraser University Library
  * Copyright (c) 2003-2014 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @class ungdomsforskningThemePlugin
- * @ingroup plugins_themes_ungdomsforskning
+ * @class tfu2ThemePlugin
+ * @ingroup plugins_themes_tfu2
  *
- * @brief "ungdomsforskning" theme plugin
+ * @brief "tfu2" theme plugin
  */
 
 import('classes.plugins.ThemePlugin');
 
-class ungdomsforskningThemePlugin extends ThemePlugin {
+class tfu2ThemePlugin extends ThemePlugin {
 	/**
 	 * Get the name of this plugin. The name must be unique within
 	 * its category.
 	 * @return String name of plugin
 	 */
 	function getName() {
-		return 'ungdomsforskningThemePlugin';
+		return 'tfu2ThemePlugin';
 	}
 
 	function getDisplayName() {
-		return 'ungdomsforskning Theme';
+		return 'tfu2 Theme';
 	}
 
 	function getDescription() {
-		return 'Responsive theme for Tidsskrift for ungdomsforskning, based on ungdomsforskning theme';
+		return 'Responsive theme for "Tidsskrift for ungdomsforskning", based on ungdomsforskning theme';
 	}
 
 	function getStylesheetFilename() {
-		return 'ungdomsforskning.css';
+		return 'tfu2.css';
 	}
 
 	function getLocaleFilename($locale) {
